406314f15a3de24cd1cd9f31c2e770fcb992199f
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-30 16:52:50 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,10 @@
 class Cat
-  # code goes here
-end
+
+  attr_reader :name, :mood 
+  
+  def initialize(name)
+    @name = name
+    @mood = mood
+  end 
+
+end 
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,27 @@
 class Owner
-  # code goes here
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  
+  
+  # def self.all 
+  #   @@all 
+  # end 
+  
+  # def self.count 
+  #   self.all.count 
+  # end 
+  
+  # def self.reset_all
+  #   self.all.clear
+  # end 
+    
+  
+  
+  
 end
